3252787867770c9f5fc6c669cf1f9c2708049dd6
tompro <tomas.prochazka@apertia.cz>
2024-07-20 14:21:49 +0200

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::process::ExitCode;
+use thiserror::Error;
+
+mod args;
+
+pub use args::CliArgs;
+
+fn main() -> ExitCode {
+    if let Err(e) = run() {
+        eprintln!("{e}");
+
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
+}
+
+fn run() -> Result<(), RunError> {
+    let _args = CliArgs::try_parse()?;
+
+    todo!()
+}
+
+#[derive(Debug, Error)]
+enum RunError {
+    #[error(transparent)]
+    ParseArgsError(#[from] clap::Error),
 }
